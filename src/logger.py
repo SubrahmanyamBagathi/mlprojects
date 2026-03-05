38e6ccd571ec0b26055f732b60c8a0669778c992
SubrahmanyamBagathi <subrahmanyam98harry@gmail.com>
2026-03-05 22:45:38 +0530

    update logger file

--- src/logger.py
+++ src/logger.py
@@ -1,16 +1,15 @@
-
-
 import logging
 import os
 from datetime import datetime
 
-
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+project_root = os.path.dirname(os.getcwd())
+logs_path = os.path.join(project_root, "logs")
 
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
@@ -18,6 +17,5 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-
-
-
+if __name__ == "__main__":
+    logging.info("logging has started")
